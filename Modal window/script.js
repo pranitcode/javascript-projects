a145aff0afa0bc2a4debc1f2bd298938b0bb4c0a
Pranit Ingole <61083651+pranitcode@users.noreply.github.com>
2021-01-09 13:19:43 +0530

    hello i am pranit

--- Modal window/script.js
+++ Modal window/script.js
@@ -1,1 +1,71 @@
 'use strict';
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector('.overlay');
+
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    
+    btnsOpenModal[i].addEventListener('click', function () {
+
+       
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+
+
+    });
+
+   
+
+
+}
+
+
+document.addEventListener('keydown', function (e) {
+
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+
+    if (e.key === "Escape") {
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+    }
+    
+});
+
+
+
+
+
+
+
+
+
+
+btnCloseModal.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+})
+overlay.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+})
+
+
+
+
+
+// const showmodal = document.querySelectorAll(".show-modal");
+// // console.log(document.querySelectorAll(".show-modal"))
+
+
+// for (let i = 0; i>showmodal.[i]; i++) {
+
+//     console.log(showmodal[i])
+
+
+
+// }
