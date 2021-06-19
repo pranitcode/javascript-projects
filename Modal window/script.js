2290d15a7c66e94ae8380f162ae037b550c0d618
Pranit Ingole <61083651+pranitcode@users.noreply.github.com>
2021-06-19 17:58:25 +0530

    Update script.js

--- Modal window/script.js
+++ Modal window/script.js
@@ -8,19 +8,10 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-    
     btnsOpenModal[i].addEventListener('click', function () {
-
-       
         modal.classList.remove('hidden');
         overlay.classList.remove('hidden');
-
-
     });
-
-   
-
-
 }
 
 
